289c8938aa4baedd047191bb7c6d64d0deef0dfb
Jan Blasko <janblasko@Jans-Mac-Pro.local>
2016-07-04 12:31:18 +0200

    Channel and icon are optional without defaults.

--- slack.lib.php
+++ slack.lib.php
@@ -2,30 +2,32 @@
 
 class slack {
 
-    public static $endpoint;
+	public static $endpoint;
 
-    // (string) $message - message to be passed to Slack
-    // (string) $room - room in which to write the message, too
-    // (string) $icon - You can set up custom emoji icons to use with each message
-    public static function alert($message, $room = "devs-room", $icon = ":warning:") {
-            $room = ($room) ? $room : "engineering";
-            $data = "payload=" . json_encode(array(
-                    "channel"       =>  "#{$room}",
-                    "text"          =>  $message,
-                    "icon_emoji"    =>  $icon
-                ));
+	// (string) $message - message to be passed to Slack
+	// (string) $channel - room in which to write the message, too
+	// (string) $icon - You can set up custom emoji icons to use with each message
+	public static function alert($message, $channel = NULL, $icon = NULL)
+	{
+		$settings = ["text" => $message];
 
-    	// You can get your webhook endpoint from your Slack settings
-            $ch = curl_init(self::$endpoint);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $result = curl_exec($ch);
-            curl_close($ch);
+		if ($channel !== NULL) {
+			$settings["channel"] = "#{$channel}";
+		}
+		if ($icon !== NULL) {
+			$settings["icon_emoji"] = "#{$icon}";
+		}
 
-    	// Laravel-specific log writing method
-            // Log::info("Sent to Slack: " . $message, array('context' => 'Notifications'));
-            return $result;
-        }
+		$data = "payload=" . json_encode($settings);
+
+		$ch = curl_init(self::$endpoint);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$result = curl_exec($ch);
+		curl_close($ch);
+
+		return $result;
+	}
 
 }
